f909975428085f01815ee05d2fde9eccb46aa337
Rajith Delantha <rajith.delantha@aurea.com>
2017-05-09 14:56:22 +0530

    Update GetAntiPatternController.java

--- src/main/java/com/antipattern/get/GetAntiPatternController.java
+++ src/main/java/com/antipattern/get/GetAntiPatternController.java
@@ -1,11 +1,18 @@
 package com.antipattern.get;
 
 import com.antipattern.service.MockService;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -47,6 +54,20 @@ public class GetAntiPatternController {
     @GetMapping(path = "/get_void_new")
     public void voidNewReturn() {
         log.info("Void new");
+    }
+    
+    @GetMapping(path = "/get_cache1")
+    public String getCache(HttpServletResponse response) {
+        response.addHeader("Cache-Control", "10");
+        return "view";
+    }
+
+    @GetMapping(path = "/get_cache2")
+    public ResponseEntity<String> getCache1() {
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(20, TimeUnit.SECONDS))
+                .body("ok");
     }
 
 
